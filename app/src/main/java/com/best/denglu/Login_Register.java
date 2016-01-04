be64cf7c3d3048c448d4545af8db96794f8f5875
810417695 <810417695@qq.com>
2016-01-05 07:36:03 +0800

    animation

--- app/src/main/java/com/best/denglu/Login_Register.java
+++ app/src/main/java/com/best/denglu/Login_Register.java
@@ -1,17 +1,37 @@
 package com.best.denglu;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.best.activity.R;
 import com.best.util.BaseActivity;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.Event;
+import org.xutils.view.annotation.ViewInject;
 
 @ContentView(R.layout.activity_login__register)
 public class Login_Register extends BaseActivity {
 
+    @ViewInject(R.id.backxxx0)
+    private ImageView backxxx0;
+    @ViewInject(R.id.backxxx1)
+    private ImageView backxxx1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.setting);
+        ImageView animationTopRightView = (ImageView)this.findViewById(R.id.backxxx0);
+        animationTopRightView.startAnimation(anim);
+        Animation anim2 = AnimationUtils.loadAnimation(this, R.anim.setting2);
+        ImageView animationTopLeftView = (ImageView)this.findViewById(R.id.backxxx1);
+        animationTopLeftView.startAnimation(anim2);
+    }
 
     @Override
     public void initView() {
