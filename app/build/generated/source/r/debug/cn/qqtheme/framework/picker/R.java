96be97455dc247ba1f02694b9956654521350e51
810417695 <810417695@qq.com>
2016-01-04 19:30:59 +0800

    3

--- app/build/generated/source/r/debug/cn/qqtheme/framework/picker/R.java
+++ app/build/generated/source/r/debug/cn/qqtheme/framework/picker/R.java
@@ -14,11 +14,11 @@ public final class R {
 		public static final int file_picker_folder = 0x7f02006e;
 		public static final int file_picker_home = 0x7f02006f;
 		public static final int file_picker_updir = 0x7f020070;
-		public static final int wheel_picker_line = 0x7f0200af;
+		public static final int wheel_picker_line = 0x7f0200b1;
 	}
 	public static final class id {
 		public static final int icon = 0x7f0d004f;
-		public static final int text = 0x7f0d00fb;
+		public static final int text = 0x7f0d00fc;
 	}
 	public static final class layout {
 		public static final int adapter_icon_text = 0x7f040031;
--- app/src/main/java/com/best/denglu/Login_Register.java
+++ app/src/main/java/com/best/denglu/Login_Register.java
@@ -1,17 +1,32 @@
 package com.best.denglu;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.best.activity.R;
 import com.best.util.BaseActivity;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.Event;
+import org.xutils.view.annotation.ViewInject;
 
 @ContentView(R.layout.activity_login__register)
 public class Login_Register extends BaseActivity {
 
+    @ViewInject(R.id.backxxx)
+    private ImageView backxxx;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(backxxx,"translationX",0.0f, 350.0f, 0f);
+        objectAnimator.setRepeatCount(-1);
+        objectAnimator.setDuration(2500).start();
+
+    }
 
     @Override
     public void initView() {
